a07289ae05be59a6c349ea49006ed82491a2087c
Apla Ganesha <157127990+AplaGanesha@users.noreply.github.com>
2026-09-12 15:14:53 +0400

    Add files via upload

---
+++ admin-config.js
@@ -0,0 +1,5 @@
+/* APLA GANESHA ADMIN CONFIG
+   IMPORTANT: GitHub Pages is static, so a password placed here is NOT true server security.
+   Change the value below to the password YOU want to use.
+*/
+window.APLA_ADMIN_PASSWORD = "CHANGE-THIS-PASSWORD";
