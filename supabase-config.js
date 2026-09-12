7b41faf76b7ea86bd0ad9ca0d962f1fcdbdcc4bb
Apla Ganesha <157127990+AplaGanesha@users.noreply.github.com>
2026-09-12 19:43:15 +0400

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://vlrexnvutirbrzvuxbln.supabase.co";
+const SUPABASE_KEY = "sb_publishable_zt93wmzyp4UU4ZJEfoBC7Q_icS3HmaH";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
