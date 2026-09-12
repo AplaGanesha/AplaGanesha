7af3a429be884a3ec403c8ccea04cfbcd0290588
Apla Ganesha <157127990+AplaGanesha@users.noreply.github.com>
2026-09-12 15:29:53 +0400

    Delete admin-config.js

--- admin-config.js
+++
@@ -1,5 +0,0 @@
-/* APLA GANESHA ADMIN CONFIG
-   IMPORTANT: GitHub Pages is static, so a password placed here is NOT true server security.
-   Change the value below to the password YOU want to use.
-*/
-window.APLA_ADMIN_PASSWORD = "BappaMorea26";
